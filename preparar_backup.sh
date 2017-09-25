55d9d5fa1ea7e0bdb8ab438421d752190d5e6954
Alexsandro de Oliveira Alves <alexsandro.alves@dataprev.gov.br>
2017-09-25 14:55:11 -0300

    Criação de preparação de arquivos para backup

---
+++ preparar_backup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#find /home/alex/projetos_dataprev/ -type d -name "target" -exec rm -Rvf {} \;
+echo "Apagando conteúdo da pasta target."
+path_code=/home/alex/projetos_dataprev
+path_sag=$path_code/sag-agu
+path_get=$path_code/get
+
+cd $path_sag/portal-atendimento-negocio
+mvn clean
+
+cd $path_sag/SagBatch
+mvn clean
+
+cd $path_sag/SagBatchSigma
+mvn clean
+
+cd $path_sag/SagGestaoAguVisao
+mvn clean
+
+cd $path_sag/SagInternetVisao
+mvn clean
+
+cd $path_sag/SagIntranetVisao
+mvn clean
+
+cd $path_sag/SagService
+mvn clean
+
+cd $path_get/get-intranet
+mvn clean
+
+cd $path_get/get-rest-api
+mvn clean
+
+path_dst_code=~/Desktop/code-sag-get
+if [ ! -d ${path_dst_code} ];then
+    echo "criando diretório de destino ${path_dst_code}"
+    mkdir -p ${path_dst_code}
+    [ $? -ne 0 ] && echo "Erro na criação de diretório" && exit 1
+fi
+
+echo "Compactando código sag"
+cd $path_code
+tar -cjf ~/Desktop/code-sag-get/sag.tar.bz2 sag-agu/
+#tar -czf ~/Desktop/code-sag-get/sag.tar.gz sag-agu/
+
+echo "Compactando código get"
+cd $path_code
+tar -cjf ~/Desktop/code-sag-get/get.tar.bz2 get/
+#tar -czf ~/Desktop/code-sag-get/get.tar.gz get/
+
+echo "FIM"
