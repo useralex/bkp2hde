560b08de0bed5c3b001f585a1938ed2a9b1c3e25
Alexsandro Alves <alexsandro.alves@gmail.com>
2018-01-02 13:25:10 -0300

    Inclusão de backup de thunderbird

--- preparar_backup.sh
+++ preparar_backup.sh
@@ -33,6 +33,9 @@ mvn clean
 cd $path_get/get-rest-api
 mvn clean
 
+cd $path_get/get-consumidores
+mvn clean
+
 path_dst_code=~/Desktop/code-sag-get
 if [ ! -d ${path_dst_code} ];then
     echo "criando diretório de destino ${path_dst_code}"
@@ -49,5 +52,16 @@ echo "Compactando código get"
 cd $path_code
 tar -cjf ~/Desktop/code-sag-get/get.tar.bz2 get/
 #tar -czf ~/Desktop/code-sag-get/get.tar.gz get/
+
+path_dst_email=~/Desktop/thunderbird
+if [ ! -d ${path_dst_email} ];then
+    echo "criando diretório de destino ${path_dst_email}"
+    mkdir -p ${path_dst_email}
+    [ $? -ne 0 ] && echo "Erro na criação de diretório" && exit 1
+fi
+
+echo "Compactando thunderbird"
+cd 
+tar -cjf Desktop/thunderbird/thunderbird.tar.bz2 .thunderbird
 
 echo "FIM"
